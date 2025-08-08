c86061142cff530a63e0f20a114691aad1ffee97
ZaydaApp <ZaydaApp@gmail.com>
2025-08-08 15:40:04 +0400

    Update offers.js

--- scripts/offers.js
+++ scripts/offers.js
@@ -5,7 +5,10 @@ let ALL = [];
 let MAP, MARKERS = [];
 
 async function loadOffers(){
-  const res = await fetch('https://script.google.com/macros/s/AKfycbwxSBWlzZj9QsNrLV0OTAMY92rcfWaia1QnfuwgOYiEeo-IRx6EKFX1Ui2veC-lL6Od1Q/exec');
+  const res = await fetch('https://script.google.com/macros/s/AKfycbwxSBWlzZj9QsNrLV0OTAMY92rcfWaia1QnfuwgOYiEeo-IRx6EKFX1Ui2veC-lL6Od1Q/exec', {
+  headers: { 'Cache-Control': 'no-cache' }
+});
+
   ALL = await res.json();
   renderOffers(ALL);
   updateTotals(ALL);
